ce93616216667c5aede46963522f82838c710a05
ArielMukesh <ariel.mukesh@gmail.com>
2016-07-20 17:41:08 +0530

    no message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $billingAddress = $customer->billingAddress;
 $subscription = $servicePortal->getSubscription();
 $customerInvoice = $servicePortal->retrieveInvoice();
 ?> 
-<div class="container container container-m-left">
+<div class="container container-m-left">
     <div id="cb-wrapper-ssp">
 
          <?php include("processing.php")  ?>
